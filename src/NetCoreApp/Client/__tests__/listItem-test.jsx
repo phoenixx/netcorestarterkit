f642bf28478fda44dd388aa99b9b78499bd2ee2c
mrose-mpd <michael.rose@mpd-group.com>
2017-02-19 22:35:52 +0000

    Made tests more succinct

--- src/NetCoreApp/Client/__tests__/listItem-test.jsx
+++ src/NetCoreApp/Client/__tests__/listItem-test.jsx
@@ -1,26 +1,28 @@
 ﻿import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import ListItem from '../components/listItem';
-//import chai from 'chai'
-//import chaiEnzyme from 'chai-enzyme'
 import { expect } from 'chai';
 import { mountWithIntl } from './helpers/intl-enzyme-test-helper';
 
-//chai.use(chaiEnzyme());
-
 describe("List item", function () {
+
+    let listItem;
+    const expectedText = "somerandomtext";
+    const expectedNumber = 7.35;
+
+    beforeEach(() => {
+        listItem = mountWithIntl(<ListItem date="2017-12-24" text={expectedText} number={expectedNumber} />);
+    });
+
     it("contains specified text",
         function () {
-            const expected = "somerandomtext";
-            const listItem = mountWithIntl(<ListItem date="2017-01-01" text={expected} number="2" />);
             const itemUnderTest = listItem.text();
             console.log(`Item under test: ${itemUnderTest}`);
-            expect(listItem.text()).to.have.string(expected);
+            expect(itemUnderTest).to.have.string(expectedText);
         });
 
     it("contains formatted currency",
         function () {
-            const listItem = mountWithIntl(<ListItem date="2017-01-01" text="test" number="7.35" />);
             const itemUnderTest = listItem.text();
             console.log(`Item under test: ${itemUnderTest}`);
             expect(itemUnderTest).to.have.string("£7.35");
@@ -28,7 +30,6 @@ describe("List item", function () {
 
     it("contains formatted date",
         function () {
-            const listItem = mountWithIntl(<ListItem date="2017-12-24" text="test" number="8" />);
             //see http://stackoverflow.com/questions/33656197/javascript-substring-check-using-indexof-or-search-on-a-date-string-with-forward
             const itemUnderTest = listItem.text().toLocaleString().replace(/\u200E/g, "");
             console.log(`Item under test: ${itemUnderTest}`);
