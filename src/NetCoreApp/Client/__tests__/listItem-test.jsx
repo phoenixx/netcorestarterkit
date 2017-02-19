a53549bcb8ec21e003eed9b1e8cb232faab42308
mrose-mpd <michael.rose@mpd-group.com>
2017-02-19 19:05:11 +0000

    Fixed tests in IE

--- src/NetCoreApp/Client/__tests__/listItem-test.jsx
+++ src/NetCoreApp/Client/__tests__/listItem-test.jsx
@@ -13,21 +13,25 @@ describe("List item", function () {
         function () {
             const expected = "somerandomtext";
             const listItem = mountWithIntl(<ListItem date="2017-01-01" text={expected} number="2" />);
-            console.log(listItem.text());
+            const itemUnderTest = listItem.text();
+            console.log(`Item under test: ${itemUnderTest}`);
             expect(listItem.text()).to.have.string(expected);
         });
 
     it("contains formatted currency",
         function () {
             const listItem = mountWithIntl(<ListItem date="2017-01-01" text="test" number="7.35" />);
-            console.log(listItem.text());
-            expect(listItem.text()).to.have.string("£7.35");
+            const itemUnderTest = listItem.text();
+            console.log(`Item under test: ${itemUnderTest}`);
+            expect(itemUnderTest).to.have.string("£7.35");
         });
 
     it("contains formatted date",
         function () {
             const listItem = mountWithIntl(<ListItem date="2017-12-24" text="test" number="8" />);
-            console.log(listItem.text());
-            expect(listItem.text()).to.have.string("24/12/2017");
+            //see http://stackoverflow.com/questions/33656197/javascript-substring-check-using-indexof-or-search-on-a-date-string-with-forward
+            const itemUnderTest = listItem.text().toLocaleString().replace(/\u200E/g, "");
+            console.log(`Item under test: ${itemUnderTest}`);
+            expect(itemUnderTest).to.have.string("24/12/2017");
         });
 });
