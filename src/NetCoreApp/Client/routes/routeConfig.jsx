f842cd2ff6a85f86d659944d8c0abc39de9d9d7b
Unknown <mik.rose@outlook.com>
2017-04-17 21:01:07 +0100

    Compile/run on linux

--- src/NetCoreApp/Client/routes/routeConfig.jsx
+++ src/NetCoreApp/Client/routes/routeConfig.jsx
@@ -1,9 +1,9 @@
 ﻿import React, { Component } from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { IntlProvider } from 'react-intl';
-import Home from '../components/Home';
+import Home from '../components/home';
 import SampleList from '../components/list';
-import Todo from '../components/todo';
+import Todo from '../components/Todo';
 
 class RouteComponent extends Component {
     constructor(props) {
--- src/NetCoreApp/preCompile.bat
+++ src/NetCoreApp/preCompile.bat
@@ -1,3 +1,4 @@
-call npm install
+#! /bin/bash
+npm install
 node node_modules/webpack/bin/webpack.js --config webpack.config.vendor.js
 node node_modules/webpack/bin/webpack.js
